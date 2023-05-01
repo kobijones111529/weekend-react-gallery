c460f30308296daae1526fa88056ba96785c8c65
Kobi Jones <kobijonesmail@gmail.com>
2023-04-30 20:17:22 -0500

    Add production database configuration for bit.io

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,20 @@
 import pg from 'pg'
 
-const pool = new pg.Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'react_gallery'
-})
+const pool = new pg.Pool(
+  process.env.NODE_ENV === 'production'
+    ? {
+        host: 'db.bit.io',
+        port: 5432,
+        database: 'kobijones/react_gallery',
+        user: 'vercel',
+        password: process.env.DB_KEY,
+        ssl: true
+      }
+    : {
+        host: 'localhost',
+        port: 5432,
+        database: 'react_gallery'
+      }
+)
 
 export default pool
